8ddee86506e230d8a5071aad60502057218fc3b5
Dido Arellano <dido@semicolonstudios.com>
2012-11-03 17:50:05 +0800

    Move zencoding config to own file

---
+++ zencoding-mode-config.el
@@ -0,0 +1,11 @@
+(add-hook 'sgml-mode-hook
+          (lambda ()
+            (require 'zencoding-mode)
+            (zencoding-mode)
+            (setq zencoding-indentation 2)
+            (define-key zencoding-mode-keymap (kbd "C-j") nil)
+            (define-key zencoding-mode-keymap (kbd "<C-return>") nil)
+            (define-key zencoding-mode-keymap (kbd "<M-return>") 'zencoding-expand-line)))
+
+
+(provide 'zencoding-mode-config)
