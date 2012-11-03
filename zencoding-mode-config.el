04d3424e57c12e6901d93706cd916373ce065ccb
Dido Arellano <dido@semicolonstudios.com>
2012-11-03 18:27:52 +0800

    Base zencoding indent on language-mode setting

--- zencoding-mode-config.el
+++ zencoding-mode-config.el
@@ -2,7 +2,7 @@
           (lambda ()
             (require 'zencoding-mode)
             (zencoding-mode)
-            (setq zencoding-indentation 2)
+            (setq zencoding-indentation sgml-basic-offset)
             (define-key zencoding-mode-keymap (kbd "C-j") nil)
             (define-key zencoding-mode-keymap (kbd "<C-return>") nil)
             (define-key zencoding-mode-keymap (kbd "<M-return>") 'zencoding-expand-line)))
