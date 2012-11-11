8b347babf2bea5abd3b2d536ce5b34ca9d10c323
Dido Arellano <dido@semicolonstudios.com>
2012-11-11 16:01:09 +0800

    Shorten evil state modeline tag

--- evil-config.el
+++ evil-config.el
@@ -1,11 +1,20 @@
 ;; Evil-mode
 (require 'evil)
-(evil-mode 1)
 
 (evil-set-initial-state 'package-menu-mode 'motion)
 
 ;; Cursor is black without this
 (setq evil-default-cursor t)
+
+(setq evil-normal-state-tag "N"
+      evil-insert-state-tag "I"
+      evil-visual-state-tag "V"
+      evil-motion-state-tag "M"
+      evil-emacs-state-tag "E"
+      evil-replace-state-tag "R"
+      evil-operator-state-tag "O")
+
+(evil-mode 1)
 
 (require 'surround)
 (global-surround-mode 1)
