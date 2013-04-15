607c41f3fd143dfacdba9afcd28d3ed47c129ca6
Dido Arellano <dido@semicolonstudios.com>
2013-04-15 23:23:05 +0800

    Moar & betterer js snippets

--- package-config/dido-yasnippet.el
+++ package-config/dido-yasnippet.el
@@ -5,11 +5,16 @@
 
 (yas-global-mode 1)
 
-(defun snippet--js-function-punctuation ()
-  (if (save-excursion
-        (search-backward "function")
-        (looking-back "= "))
-      ";" ""))
+(defun yas-js-semicolon-or-parens ()
+  (save-excursion
+    (search-backward "function")
+    (cond
+     ((looking-back "= ") ";")
+     ;; passing anon as callback turns into iife as callback. That's wrong.
+     ;; ((looking-back "somechar(") "dido-insert-semicolon-at-eol")
+     ((or (looking-back "(") (looking-back "= ("))
+      (dido-insert-semicolon-at-eol) "()")
+     (t ""))))
 
 
 (provide 'dido-yasnippet)
