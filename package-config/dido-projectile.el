89a2974c67f51b4eeef8be5e80bfbf72fc296c67
Dido Arellano <dido@semicolonstudios.com>
2013-10-14 20:24:26 +0800

    Move projectile bookmarks file to tmp dir

--- package-config/dido-projectile.el
+++ package-config/dido-projectile.el
@@ -3,6 +3,8 @@
 (projectile-global-mode)
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
 (add-to-list 'projectile-globally-ignored-directories "elpa")
+(setq projectile-known-projects-file
+      (expand-file-name "projectile-bookmarks.eld" dido-tmp-directory))
 
 (global-set-key (kbd "C-c f") 'dido-do-find-file-projectile-or-ido)
 
