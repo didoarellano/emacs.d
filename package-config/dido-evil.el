182187b0ed003ad286adae477a85d23140f2ac2e
Dido Arellano <dido@semicolonstudios.com>
2013-10-16 13:32:25 +0800

    Change evil-numbers bindings*
    We use (and are used to) too many C-x emacs functions

--- package-config/dido-evil.el
+++ package-config/dido-evil.el
@@ -21,7 +21,7 @@
 (global-surround-mode 1)
 
 (require 'evil-numbers)
-(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+(define-key evil-normal-state-map (kbd "C-=") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C--") 'evil-numbers/dec-at-pt) ; was negative-argument
 
 (provide 'dido-evil)
