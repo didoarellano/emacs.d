e29098dada536fe02f32b044f7a7649ea356a1c3
Dido Arellano <dido@semicolonstudios.com>
2014-02-25 18:01:28 +0800

    Add color toggling to modeline

--- dido-modeline.el
+++ dido-modeline.el
@@ -18,6 +18,22 @@
   '((t (:background "#222222" :inherit powerline-normal))) "")
 (defface powerline-inactive
   '((t (:slant italic :background "#222222" :foreground "#aaaaaa"))) "")
+(defface powerline-evil-red
+  '((t (:background "#e74c3c" :foreground "#111111"))) "")
+(defface powerline-evil-green
+  '((t (:background "#2ecc71" :foreground "#111111"))) "")
+(defface powerline-evil-blue
+  '((t (:background "#5f87af" :foreground "#eeeeee"))) "")
+
+(defun get-evil-state-color (state is-active-window)
+  (let ((powerline-evil-red-states `("I" "E" "R"))
+        (powerline-evil-green-states `("V" "O"))
+        (powerline-evil-blue-states `("N" "M")))
+    (cond
+     ((and is-active-window (member state powerline-evil-blue-states)) 'powerline-evil-blue)
+     ((member state powerline-evil-red-states) 'powerline-evil-red)
+     ((member state powerline-evil-green-states) 'powerline-evil-green)
+     (t 'powerline-inactive))))
 
 (defpowerline powerline-vcs
   (let ((str " ±" ))
@@ -51,14 +67,17 @@
                         (dark (if active 'powerline-dark-active 'powerline-inactive))
                         (darker (if active 'powerline-darker-active 'powerline-inactive))
                         (evil (if active 'powerline-evil-state-active 'powerline-inactive))
+                        (state-color (get-evil-state-color evil-mode-line-tag active))
+                        (buffer-color (if (and (buffer-file-name)
+                                               (file-exists-p (buffer-file-name))
+                                               (buffer-modified-p))
+                                          'powerline-evil-red bright))
 
                         (lhs (list
 
-                              (powerline-raw "%*" bright 'l)
-
-                              (powerline-buffer-id bright 'l)
-                              (powerline-raw " " bright)
-                              (powerline-arrow-left bright light)
+                              (powerline-buffer-id buffer-color 'l)
+                              (powerline-raw " " buffer-color)
+                              (powerline-arrow-left buffer-color light)
 
                               (powerline-vcs light)
                               (powerline-arrow-left light dark)
@@ -71,10 +90,9 @@
 
                         (rhs (list
 
-                              (powerline-arrow-right 'powerline-normal light)
-
-                              (powerline-evil-state evil 'r)
-                              (powerline-arrow-right light darker)
+                              (powerline-arrow-right 'powerline-normal state-color)
+                              (powerline-evil-state state-color 'r)
+                              (powerline-arrow-right state-color darker)
 
                               (powerline-raw "%4l" darker 'r)
                               (powerline-raw ":" darker)
