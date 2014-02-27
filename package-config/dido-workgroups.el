121e6f1435ed8e62aa5921963adf9978e640b1a9
Dido Arellano <dido@semicolonstudios.com>
2014-02-27 13:33:37 +0800

    Workaround *Help* buffer session-saving bug
    The bug:
    pashinin/workgroups2#29
    
    The workaround:
    Kill help buffers before saving session.

--- package-config/dido-workgroups.el
+++ package-config/dido-workgroups.el
@@ -2,6 +2,13 @@
 
 (setq wg-default-session-file "~/Dropbox/emacs/workgroups")
 
+(defadvice wg-save-session (around dont-save-help-buffers-advice activate)
+  (kill-help-buffers) ad-do-it)
+
+(defun kill-help-buffers ()
+  (dolist (buffer (buffer-list))
+    (if (string= "*Help*" (buffer-name buffer))
+        (kill-buffer buffer))))
 
 (defun activate-workgroups-mode()
   (workgroups-mode 1))
