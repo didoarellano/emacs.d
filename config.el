62980f45110dd427a0916fd534a3c51c72a9f5df
Dido L. Arellano <didoarellano@gmail.com>
2020-05-12 21:41:42 +0800

    Shush Beginning & End of buffer messages

--- config.el
+++ config.el
@@ -127,3 +127,17 @@ end of the line, then comment or uncomment the current line."
 
 (after! evil
   (setq evil-echo-state nil))
+
+;; Suppress "Beginning of buffer" and "End of buffer" messages
+;; https://superuser.com/a/1025827/182507
+(defadvice previous-line (around silencer activate)
+  (condition-case nil
+    ad-do-it
+    ((beginning-of-buffer))))
+(defadvice next-line (around silencer activate)
+  (condition-case nil
+    ad-do-it
+    ((end-of-buffer))))
+;; The above works but this answer might be more future-proof and can work for
+;; other messages we want to suppress.
+;; https://superuser.com/a/1077571/182507
