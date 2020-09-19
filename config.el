ee1122d0f4e147afe9e2633e637487abbf705585
Dido L. Arellano <didoarellano@gmail.com>
2020-09-19 16:05:33 +0800

    Extract cycle-case into own package

--- config.el
+++ config.el
@@ -96,36 +96,10 @@ end of the line, then comment or uncomment the current line."
                                  default-directory)) nil 0))
 (map! "<f12>" `--open-terminal-here)
 
-;; Case cycling
-;; http://lists.gnu.org/archive/html/help-gnu-emacs/2008-10/msg00677.html
-(defun --cycle-letter-case ()
-  "Toggle the letter case of current word or text selection. Toggles from 3
-    cases: UPPER CASE, lower case, Title Case, in that cyclic order."
-  (interactive)
-  (let (pos1 pos2 (deactivate-mark nil) (case-fold-search nil))
-    (if (and transient-mark-mode mark-active)
-        (setq pos1 (region-beginning)
-              pos2 (region-end))
-      (setq pos1 (car (bounds-of-thing-at-point 'word))
-            pos2 (cdr (bounds-of-thing-at-point 'word))))
-
-    (when (not (eq last-command this-command))
-      (save-excursion
-        (goto-char pos1)
-        (cond
-         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
-         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps"))
-         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps"))
-         (t (put this-command 'state "all lower")))))
-
-    (cond
-     ((string= "all lower" (get this-command 'state))
-      (upcase-initials-region pos1 pos2) (put this-command 'state "init caps"))
-     ((string= "init caps" (get this-command 'state))
-      (upcase-region pos1 pos2) (put this-command 'state "all caps"))
-     ((string= "all caps" (get this-command 'state))
-      (downcase-region pos1 pos2) (put this-command 'state "all lower")))))
-(map! :v "~" `--cycle-letter-case)
+(use-package cycle-case
+  :load-path "~/src/emacs.d/packages/cycle-case"
+  :bind (:map evil-visual-state-map
+          ("~" . cycle-case)))
 
 (map! "C-?" 'help-command
       :i "C-h" 'backward-delete-char    ; was help-command
---
+++ packages/cycle-case/cycle-case.el
@@ -0,0 +1,32 @@
+;;; ~/src/emacs.d/packages/cycle-case/cycle-case.el -*- lexical-binding: t; -*-
+
+;; http://lists.gnu.org/archive/html/help-gnu-emacs/2008-10/msg00677.html
+(defun cycle-case ()
+  "Toggle the letter case of current word or text selection. Toggles from 3
+    cases: UPPER CASE, lower case, Title Case, in that cyclic order."
+  (interactive)
+  (let (pos1 pos2 (deactivate-mark nil) (case-fold-search nil))
+    (if (and transient-mark-mode mark-active)
+        (setq pos1 (region-beginning)
+              pos2 (region-end))
+      (setq pos1 (car (bounds-of-thing-at-point 'word))
+            pos2 (cdr (bounds-of-thing-at-point 'word))))
+
+    (when (not (eq last-command this-command))
+      (save-excursion
+        (goto-char pos1)
+        (cond
+         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
+         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps"))
+         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps"))
+         (t (put this-command 'state "all lower")))))
+
+    (cond
+     ((string= "all lower" (get this-command 'state))
+      (upcase-initials-region pos1 pos2) (put this-command 'state "init caps"))
+     ((string= "init caps" (get this-command 'state))
+      (upcase-region pos1 pos2) (put this-command 'state "all caps"))
+     ((string= "all caps" (get this-command 'state))
+      (downcase-region pos1 pos2) (put this-command 'state "all lower")))))
+
+(provide 'cycle-case)
